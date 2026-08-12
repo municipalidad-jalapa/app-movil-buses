18326c62ad8498af30a8a2496f6d2a341827a741
ALLAN GABRIEL AGUILAR REYES <aaguilarr16@miumg.edu.gt>
2026-08-11 19:36:35 -0600

    chore: arranque proyecto spring boot con postgis (HU-17)

---
+++ src/main/java/gt/muni/jalapa/ecoruta/EcoRutaApplication.java
@@ -0,0 +1,12 @@
+package gt.muni.jalapa.ecoruta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoRutaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoRutaApplication.class, args);
+    }
+}
