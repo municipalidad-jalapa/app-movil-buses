7fc60759fa6391fbc8ca444393074c30476c7af5
JoseCabrera88 <105029340+JoseCabrera88@users.noreply.github.com>
2026-08-11 20:55:51 -0600

    docs: swagger, coleccion postman y README (HU-25)

---
+++ src/main/java/gt/gob/jalapa/buses/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package gt.muni.jalapa.ecoruta.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI ecoRutaOpenAPI() {
+        return new OpenAPI().info(new Info()
+                .title("EcoRuta API")
+                .description("Plataforma del bus electrico municipal de Jalapa")
+                .version("v1"));
+    }
+}
