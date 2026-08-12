e6589be9b17a0e63fbbf8d07c0ee683ee7605e94
JoseCabrera88 <105029340+JoseCabrera88@users.noreply.github.com>
2026-08-11 21:41:56 -0600

    fix: add SpringDoc dependency and correct OpenAPI package

--- src/main/java/gt/gob/jalapa/buses/config/OpenApiConfig.java
+++ src/main/java/gt/gob/jalapa/buses/config/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package gt.muni.jalapa.ecoruta.config;
+package gt.gob.jalapa.buses.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -12,7 +12,7 @@ public class OpenApiConfig {
     public OpenAPI ecoRutaOpenAPI() {
         return new OpenAPI().info(new Info()
                 .title("EcoRuta API")
-                .description("Plataforma del bus electrico municipal de Jalapa")
+                .description("Plataforma del bus eléctrico municipal de Jalapa")
                 .version("v1"));
     }
 }
